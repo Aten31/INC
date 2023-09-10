e38f3fce02cf9b32a8841f12418d59019178d094
Aten31 <130209757+Aten31@users.noreply.github.com>
2023-09-10 22:33:34 +0545

    Add files via upload

--- main.js
+++ main.js
@@ -3,3 +3,24 @@
 window.addEventListener('scroll',() =>{
     document.querySelector('nav').classList.toggle('window-scroll',window.scrollY > 0)
 })
+
+
+// show/hide faq answers
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faqs => {
+    faqs.addEventListener('click',() =>{
+        faqs.classList.toggle('open');
+    
+    //change icon
+    const icon = faq.querySelector('.faq-icon i');
+    if(icon.classname === 'uli uli-plus'){
+        icon.classname = "uli uli-minus";
+    }else {
+        icon.classname ="uli uli-plus";
+    }
+    })
+
+
+})
